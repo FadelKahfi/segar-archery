b51585bb727705b85aae953e4dd1619eeab78b59
fatahillah2606 <muhamad.fatahillah@raharja.info>
2026-02-18 00:10:05 +0700

    Update
    - Working on login system
    - Updated .gitignore
    - Installed flowbite but not implemented yet
    - Updated requirements.txt

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
-from flask import Flask, render_template, jsonify, request, redirect, url_for, session, send_from_directory
-from datetime import timedelta, datetime, timezone
-from dotenv import load_dotenv
-import os, requests, json
+import json
+import os
+from datetime import datetime, timedelta, timezone
 
+import bcrypt
+import requests
+from dotenv import load_dotenv
+from flask import (
+    Flask,
+    jsonify,
+    redirect,
+    render_template,
+    request,
+    send_from_directory,
+    session,
+    url_for,
+)
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
-from dotenv import load_dotenv
-import os
+from requests.sessions import Request
 
 # Load .env
 load_dotenv()
@@ -15,6 +26,7 @@ load_dotenv()
 DB_HOST = os.getenv("DB_HOST")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
+
 
 def get_client():
     # URI ke Atlas
@@ -27,19 +39,23 @@ def get_client():
         return client
     except Exception as e:
         print(f"Gagal connect Atlas: {e}")
-        
+
+
 # Client aktif
 client = get_client()
+
 
 # Database
 def use_db():
     return client["SegarArcheryDB"]
+
 
 # Collection
 collection = use_db()
 userData = collection["coaches"]
 
 app = Flask(__name__)
+
 
 # Respon API
 def respon_api(status, code, message, data, pagination):
@@ -48,51 +64,52 @@ def respon_api(status, code, message, data, pagination):
         "code": code,
         "message": message,
         "data": data if data else [],
-        "pagination": pagination if pagination else {}
+        "pagination": pagination if pagination else {},
     }
     return jsonify(respon)
 
+
 # Coach Login
-@app.route('/api/coach/login', methods=["POST", "GET"])
+@app.route("/api/coach/login", methods=["POST", "GET"])
 def coachLogin():
-    
     try:
         if request.method == "POST":
-            
-            data_json = request.json()
-            print(data_json)
-            username = ""
-            password = ""
-            return respon_api("success", 200, "Sukses", [], {})
-            
+            username = str(request.form["username"])
+            password = str(request.form["password"])
+
             # Check Account
-            # account = userData.find_one({"username": username})
+            account = userData.find_one({"username": username})
 
-            # if account:
-            #     dbpw = account["password"]
-            #     dbpw = dbpw.encode("utf-8")
-            #     encodepw = password.encode("utf-8")
+            if account:
+                dbpw = account["password"]
+                dbpw = dbpw.encode("utf-8")
+                encodepw = password.encode("utf-8")
 
-            #     valid = bcrypt.checkpw(encodepw, dbpw)
-            #     if valid:
-            #        return respon_api("success", 200, "Sukses", [], {})
-            #     else:
-            #         return respon_api("failed", 404, "Tetot", [], {}), 404
+                valid = bcrypt.checkpw(encodepw, dbpw)
+                if valid:
+                    return respon_api("success", 200, "Sukses", [], {})
+                else:
+                    return respon_api("failed", 404, "Tetot", [], {}), 404
+
+            else:
+                return respon_api("failed", 404, "Account not found", [], {}), 404
         else:
-            return respon_api("error", 200, "Req Method not POST", [], {})
-                
+            return respon_api("error", 400, "Req Method not POST", [], {}), 400
+
     except Exception as error:
-       return respon_api("error", 500, str(error), [], {}), 500
+        return respon_api("error", 500, str(error), [], {}), 500
 
 
 # Home pages
-@app.route('/coach')
+@app.route("/coach")
 def coach():
-  return render_template('coach/index.html')
+    return render_template("coach/index.html")
 
-@app.route('/')
+
+@app.route("/")
 def home():
-  return render_template('index.html')
+    return render_template("index.html")
+
 
 if __name__ == "__main__":
-  app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0", debug=True)
